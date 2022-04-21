51973969c648b084dbc6e8d7645fe3ea8651816d
funkycram <contact@marc.fun>
2022-04-21 12:16:03 +0200

    update

--- views/container-config/index.php
+++ views/container-config/index.php
@@ -68,7 +68,7 @@ $module = Yii::$app->getModule('rocket');
                 </div>
                 <div class="panel-body">
                     <p><?= Yii::t('RocketModule.config', 'Allow Humhub to be embedded in Rocket.chat: in the {contentSecurityPolicy}, you should have:', ['contentSecurityPolicy' => '"Content Security Policy"']) ?></p>
-                    <p><code>frame-ancestors 'self' to frame-ancestors 'self' <?= Url::base(true) ?></code></p>
+                    <p><code>frame-ancestors 'self' <?= Url::base(true) ?>;</code></p>
                     <?php $rocketUrl = $module->settings->get('apiUrl') . '/admin/Layout'; ?>
                     <p><?= Yii::t('RocketModule.config', 'Go to {rocketUrl} -> "Custom Scripts". And in {buttonName} add:', ['rocketUrl' => Html::a($rocketUrl, $rocketUrl, ['target' => '_blank']), 'buttonName' => '"Custom Script for Logged In Users"']) ?></p>
                     <pre><code>
